98df674eba385a0fb0796e0bdc220c463f95d076
phillipivan <phillip.pietruschka@parliament.vic.gov.au>
2023-11-06 18:18:28 +1100

    Remove redundant def.

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ const MinChannelCount = 8
 const GroupCount = 3
 const MatrixCount = 6
 const EndSession = 'QUIT\r\n'
-// eslint-disable-next-line no-unused-vars
-let regexp_SafeString = new RegExp(/^[^,*;]{1,15}/g)
 let automixChannels = []
 for (let i = MinChannelCount; i <= MaxChannelCount; i++) {
 	automixChannels.push({ id: i, label: i + ' Automix Channels' })
